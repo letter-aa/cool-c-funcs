01a779b3f6687f7f445206ba86cb5b085072ad37
a <102840087+letter-aa@users.noreply.github.com>
2023-12-31 18:51:26 -0500

    Update coolCFuncs.h

--- coolCFuncs.h
+++ coolCFuncs.h
@@ -3,15 +3,22 @@
 #define FUNCTION(NAME, TYPE, ...) TYPE(*NAME)(__VA_ARGS__)
 #define ARRLEN(ARRAY) sizeof(ARRAY) / sizeof(ARRAY[0])
 
+#ifdef _WIN32
+typedef int(__cdecl* any)();
+#endif
+
+int* sort(int* array, int arrayLen, int* lpRetSize);
 char* reverse(char* main);
-char* substr(char* main, int start, int length);
-char** splitByByte(char* main, int splitBy);
-int numofstr(char* main, char* lookfor);
+char* substr(char* main, int start);
+char* substrEx(char* main, int start, int length);
 int find(char* main, char* find);
+int findNth(char* main, char* findStr, int nth);
+char** splitByByte(char* main, int splitBy, int* sizeOfResult);
+int numofstr(char* main, char* lookfor);
+char** split(char* main, char* splitBy, int* sizeOfResult);
 int find_last_of(char* main, char* find);
 int find_first_not_of(char* main, char* find);
 int find_last_not_of(char* main, char* find);
-char** split(char* main, char* splitBy);
 char* oldTrim(char* main);
 char* trim(char* main);
 char* itos(int i);
