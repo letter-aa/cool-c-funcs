a68754b111861b9999a58014b459571e1998dfde
a <102840087+letter-aa@users.noreply.github.com>
2024-01-04 13:12:05 -0500

    Update coolCFuncs.c

--- coolCFuncs.c
+++ coolCFuncs.c
@@ -8,9 +8,57 @@
 
 typedef long long ll;
 
+// thank you to FreakAnon for making getarch()
+// i couldnt make this cuz i dont rlly know that much about architectures lmao
+string getarch() {
+#if defined(__x86_64__) || defined(_M_X64)
+    return "x86_64";
+#elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+    return "x86_32";
+#elif defined(__ARM_ARCH_2__)
+    return "ARM2";
+#elif defined(__ARM_ARCH_3__) || defined(__ARM_ARCH_3M__)
+    return "ARM3";
+#elif defined(__ARM_ARCH_4T__) || defined(__TARGET_ARM_4T)
+    return "ARM4T";
+#elif defined(__ARM_ARCH_5_) || defined(__ARM_ARCH_5E_)
+    return "ARM5"
+#elif defined(__ARM_ARCH_6T2_) || defined(__ARM_ARCH_6T2_)
+    return "ARM6T2";
+#elif defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__)
+    return "ARM6";
+#elif defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+    return "ARM7";
+#elif defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+    return "ARM7A";
+#elif defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+    return "ARM7R";
+#elif defined(__ARM_ARCH_7M__)
+    return "ARM7M";
+#elif defined(__ARM_ARCH_7S__)
+    return "ARM7S";
+#elif defined(__aarch64__) || defined(_M_ARM64)
+    return "ARM64";
+#elif defined(mips) || defined(__mips__) || defined(__mips)
+    return "MIPS";
+#elif defined(__sh__)
+    return "SUPERH";
+#elif defined(__powerpc) || defined(__powerpc__) || defined(__powerpc64__) || defined(__POWERPC__) || defined(__ppc__) || defined(__PPC__) || defined(_ARCH_PPC)
+    return "POWERPC";
+#elif defined(__PPC64__) || defined(__ppc64__) || defined(_ARCH_PPC64)
+    return "POWERPC64";
+#elif defined(__sparc__) || defined(__sparc)
+    return "SPARC";
+#elif defined(__m68k__)
+    return "M68K";
+#else
+    return "UNKNOWN";
+#endif
+}
+
 int* sort(int* array, int arrayLen, int* sizeOfResult) {
     int* arr = array;
-    int* res = malloc(arrayLen * sizeof(int));
+    int* res = malloc(arrayLen * sizeof(int*));
     int ex1 = -1;
     //int ex2 = -1;
     int j = 0;
@@ -44,7 +92,7 @@ int* sort(int* array, int arrayLen, int* sizeOfResult) {
 
 char* input() {
     char* input;
-    input = (char*)malloc(sizeof(char));
+    input = malloc(sizeof(char*));
     char c = getchar();
     int j = 0;
     while (c != '\n') {
@@ -63,8 +111,8 @@ char* input() {
 
 frequency frequencies(char* string) {
     int size = strlen(string); //size of input
-    int* freq = (int*)malloc(size * sizeof(int)); //num of frequencies
-    char* str = (char*)malloc(size * sizeof(char)); //characters associated with the number of frequencies in the freq array
+    int* freq = malloc(size * sizeof(int*)); //num of frequencies
+    char* str = malloc(size * sizeof(char*)); //characters associated with the number of frequencies in the freq array
     int j = 0;
     bool found = false;
 
